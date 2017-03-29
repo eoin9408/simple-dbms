81b6612acdeb475b80927d4cf5f7c6c9ad4e2076
eoin9408 <eoin9408@gmail.com>
2017-03-30 00:52:58 +0100

    Added to DatabaseService class.

---
+++ simple-dbms/simple-dbms/DatabaseService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace simple_dbms
+{
+    public class DatabaseService
+    {
+        public IDatabase db { get; set; }
+
+        public DatabaseService(IDatabase db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<string> GetDatabases()
+        {
+            int columnIndex = 0;
+            List<string> result = new List<string>();
+            DataTable table = db.ExecuteQuery("SELECT * FROM sys.databases");
+            for(int i = 0; i < table.Columns.Count; i++)
+            {
+                if(table.Columns[i].ColumnName == "name")
+                {
+                    columnIndex = i;
+                    break;
+                }
+            }
+
+            for(int c = 0; c < table.Rows.Count; c++)
+            {
+                result.Add((string)table.Rows[c].ItemArray[columnIndex]);
+            }
+            
+            return result;
+
+            // look through table, return database names
+        }
+    }
+}
--- simple-dbms/simple-dbms/MainWindow.xaml.cs
+++ simple-dbms/simple-dbms/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace simple_dbms
     {
         public MainWindow()
         {
+            DatabaseService dbs
             InitializeComponent();
         }
     }
