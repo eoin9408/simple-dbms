5a4c2b8f9a3bd9b14f9f424f32929816c7533923
eoin9408 <eoin9408@gmail.com>
2017-03-29 23:31:05 +0100

    Added Database and People classes.

---
+++ simple-dbms/simple-dbms/People.cs
@@ -0,0 +1,21 @@
+﻿namespace simple_dbms
+{
+    public class People
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public People() { }
+
+        public People(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.id}, {this.name}";
+        }
+    }
+}
